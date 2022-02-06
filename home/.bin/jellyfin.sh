80bf089f9271cb9441d2d6e1fb3a20f9faf0e763
ThunderThief <thunderthief5@gmail.com>
2022-02-06 19:50:21 +0530

    Added Jellyfin folders

---
+++ home/.bin/jellyfin.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+JELLYFINDIR="$HOME/Apps/jellyfin"
+
+$JELLYFINDIR/jellyfin/jellyfin \
+ -d $JELLYFINDIR/data \
+ -C $JELLYFINDIR/cache \
+ -c $JELLYFINDIR/config \
+ -l $JELLYFINDIR/log \
+ > /dev/null 2>&1
+ 
